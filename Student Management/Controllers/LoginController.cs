f7ef0937de1e36479b8cdb9b53bbad3f15004d97
Jithmini <jith4ever@gmail.com>
2020-02-13 11:17:39 +0530

    Commited student management system

---
+++ Student Management/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Student_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management.Controllers
+{
+    public class LoginController : Controller
+    {
+        StudentManagementEntities db = new StudentManagementEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User obj)
+        {
+            using(StudentManagementEntities db=new StudentManagementEntities())
+            {
+                var userdetails = db.Users.Where(x => x.UseName == obj.UseName && x.Password == obj.Password).FirstOrDefault();
+
+                if (userdetails != null)
+                {
+                    Session["UserID"] = userdetails.ID.ToString();
+                    Session["UserName"] = userdetails.UseName.ToString();
+
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password Incorrect");
+                }
+            }
+
+            
+            return View(obj);
+            
+        }
+
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
